1b155bb543c4f9ecd0b13da790196798834f832c
Stanley Fok <stanleyfok@gmail.com>
2019-11-27 05:42:48 +0800

    add build version to docs folder

---
+++ docs/precache-manifest.f928ad531a47e24c39494cb9945ad0fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba915bda92f921e3f04ad611e2f75e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844f7667703da69c309e",
+    "url": "/static/css/main.2d18d313.chunk.css"
+  },
+  {
+    "revision": "290f318c4ede0acf2199",
+    "url": "/static/js/2.2d718703.chunk.js"
+  },
+  {
+    "revision": "844f7667703da69c309e",
+    "url": "/static/js/main.b3e16993.chunk.js"
+  },
+  {
+    "revision": "ab5969a430f4388e3947",
+    "url": "/static/js/runtime-main.0b4789b9.js"
+  },
+  {
+    "revision": "4ae866dc425a9904e84252322ec8413b",
+    "url": "/static/media/tetris-bg.4ae866dc.mp3"
+  }
+]);
